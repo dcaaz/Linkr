72ddeddf4fa1e4b9c0ea2e33797201eb6da830eb
andherpaulo <andher.dev@gmail.com>
2023-01-05 11:50:33 -0300

    refact: move /hashtag/:hashtag route to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { getPostByHashtag } from "../controllers/hashtagControllers.js";
 import { getUserById } from "../controllers/usersController.js";
 import validateAuthorization from "../middlewares/validateAuthorizationMiddleware.js";
 import postsRouter from "./postsRoutes.js";
@@ -6,6 +7,7 @@ import routeSign from "./signRoutes.js";
 
 const router = Router();
 router.use("/posts", postsRouter);
+router.get("/hashtag/:hashtag", getPostByHashtag);
 router.get("/users/:id", validateAuthorization, getUserById);
 router.use(routeSign);
 
