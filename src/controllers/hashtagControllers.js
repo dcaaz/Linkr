1d913bcce72c64582a4a82f849a64b2c1afda907
Giovanni Padova <gpadova@belmare.com.br>
2023-01-05 18:44:49 -0300

    feat: likes and dislikes

--- src/controllers/hashtagControllers.js
+++ src/controllers/hashtagControllers.js
@@ -1,19 +1,41 @@
-// importar banco de dados
+import connectionDB from "../database/db"
 
 export async function getPostByHashtag(req, res) {
     const {hashtag} = req.params
 
     try {
-        const twitts = await connectionDb.query(`
+        const twitts = await connectionDB.query(`
         SELECT users.username, users.picture_url, posts.description, posts.link, posts.likes_amount
         FROM posts_hashtags
-            JOIN hastags ON hashtags.id = post_hashtags.hashtag_id
+            JOIN hashtags ON hashtags.id = post_hashtags.hashtag_id
             JOIN posts ON posts_hashtags.post_id = posts.id
             JOIN users ON users.id = posts.user_id 
         WHERE hashtags.name = $1
         `, [hashtag])
 
         res.send(twitts).status(201)
+        
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+}
+
+export async function getHashtags(req, res) {
+
+
+    try {
+        const hashtags = await connectionDB.query(`
+        SELECT hashtags.name  SUM(hashtags.*) AS likes_sum
+        FROM posts_hashtags
+            JOIN hashtags ON hashtags.id = post_hashtags.hashtag_id
+            JOIN posts ON posts_hashtags.post_id = posts.id
+        GROUP BY posts_hashtags.hashtag_id
+        ORDER BY likes_sum DESC
+        LIMIT 10;
+        `)
+
+        res.send(hashtags).status(201)
         
     } catch (error) {
         console.log(error)
