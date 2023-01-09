1626624d089a886fd4312fddb0ab2672672d0a6b
andherpaulo <andher.dev@gmail.com>
2023-01-09 10:15:16 -0300

    feat: add get /users route

--- src/repositories/usersRepository.js
+++ src/repositories/usersRepository.js
@@ -2,13 +2,20 @@ import connectionDB from "../database/db.js";
 
 const UsersRepository = {
     selectUserById: async (id) => {
-        const user = await connectionDB.query(
+        const { rows } = await connectionDB.query(
             `SELECT *
             FROM users
             WHERE id = $1;`,
             [id]
         );
-        return user.rows[0];
+        return rows[0];
+    },
+    selectAllUsers: async () => {
+        const { rows } = await connectionDB.query(
+            `SELECT *
+            FROM users;`
+        );
+        return rows;
     },
 };
 
