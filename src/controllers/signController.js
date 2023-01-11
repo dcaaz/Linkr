ef31797079c11aa1b747fb652b55f3c92dce55b7
andherpaulo <andher.dev@gmail.com>
2023-01-11 17:13:21 -0300

    fix: catch metascraper errors

--- src/controllers/signController.js
+++ src/controllers/signController.js
@@ -1,22 +1,23 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-import { insertUsers, selectIdFromUsers, insertSessions } from "../repositories/signRepository.js";
+import {
+    insertUsers,
+    selectIdFromUsers,
+    insertSessions,
+} from "../repositories/signRepository.js";
 
 export async function postSignup(req, res) {
-
     const dataSignup = req.dataUser;
 
     try {
-
         let encryptPassword = bcrypt.hashSync(dataSignup.password, 12);
 
         await insertUsers(dataSignup, encryptPassword);
 
         return res.sendStatus(201);
-
     } catch (err) {
         console.log("err postSignup", err.message);
-        return res.status(500).send('Server not running');
+        return res.status(500).send("Server not running");
     }
 }
 
@@ -26,16 +27,13 @@ export async function postSignin(req, res) {
     const token = uuid();
 
     try {
-
         const userId = await selectIdFromUsers(dataSignin);
 
         await insertSessions(userId, token);
-        
-        return res.status(200).send([ token, all]);
-    }
-    catch (err) {
+
+        return res.status(200).send([token, all]);
+    } catch (err) {
         console.log("err postSignin", err.message);
-        return res.status(500).send('Server not running');
+        return res.status(500).send("Server not running");
     }
 }
-
--- src/middlewares/signinMiddleware.js
+++ src/middlewares/signinMiddleware.js
@@ -1,45 +1,53 @@
 import connectionDB from "../database/db.js";
 import { signinSchema } from "../models/signinModel.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 export async function signinValidation(req, res, next) {
-
     const dataSignin = req.body;
 
     try {
-
-        const { error } = signinSchema.validate(dataSignin, { abortEarly: false });
+        const { error } = signinSchema.validate(dataSignin, {
+            abortEarly: false,
+        });
 
         if (error) {
-            const errors = error.details.map(detail => detail.message);
+            const errors = error.details.map((detail) => detail.message);
             return res.status(422).send(errors);
         }
 
-        const userExist = await connectionDB.query("SELECT * FROM users WHERE email=$1", [dataSignin.email]);
+        const userExist = await connectionDB.query(
+            "SELECT * FROM users WHERE email=$1",
+            [dataSignin.email]
+        );
 
-        const all = userExist.rows[0]
-       
+        const all = userExist.rows[0];
+
         if (userExist.rowCount === 0) {
             return res.status(409).send("E-mail incorreto, insira novamente");
         }
 
-        const passwordExist = await connectionDB.query('SELECT password FROM users WHERE email=$1;', [dataSignin.email]);
+        const passwordExist = await connectionDB.query(
+            "SELECT password FROM users WHERE email=$1;",
+            [dataSignin.email]
+        );
 
         const passwordCrypted = passwordExist.rows[0].password;
 
-        const passwordOk = bcrypt.compareSync(dataSignin.password, passwordCrypted);
+        const passwordOk = bcrypt.compareSync(
+            dataSignin.password,
+            passwordCrypted
+        );
 
         if (!passwordOk) {
-            return res.status(401).send("Senha incorreta, insira novamente");;
-        };
+            return res.status(401).send("Senha incorreta, insira novamente");
+        }
 
         req.dataUser = dataSignin;
         req.dataAll = all;
-        
-        next();
 
+        next();
     } catch (err) {
         console.log("err signinValidation", err.message);
-        res.status(500).send('Server not running');
+        res.status(500).send("Server not running");
     }
 }
